6351ac5aa702e36b2f84ace3c31555470cb83230
jiuhao <815514981@qq.com>
2024-08-28 14:55:56 +0800

    update

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,15 +1,12 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	version = "*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		-- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
-    config = function()
-    require("neo-tree").setup()
-    end,
+	config = function()
+		require("neo-tree").setup()
+	end,
 }
