79409573f3bf924095937294b507e64e3bace708
jiuhao <815514981@qq.com>
2024-08-30 11:28:49 +0800

    update

--- lua/custom/plugins/mini-surround.lua
+++ lua/custom/plugins/mini-surround.lua
@@ -1,18 +1,28 @@
+-- return {
+-- 	{
+-- 		{
+-- 			"echasnovski/mini.surround",
+-- 			opts = {
+-- 				mappings = {
+-- 					add = ";;",
+-- 					delete = ";d",
+-- 					find = ";f",
+-- 					find_left = ";F",
+-- 					highlight = ";h",
+-- 					replace = ";r",
+-- 					update_n_lines = ";n",
+-- 				},
+-- 			},
+-- 		},
+-- 	},
+-- }
 return {
-	{
-		{
-			"echasnovski/mini.surround",
-			opts = {
-				mappings = {
-					add = ";;",
-					delete = ";d",
-					find = ";f",
-					find_left = ";F",
-					highlight = ";h",
-					replace = ";r",
-					update_n_lines = ";n",
-				},
-			},
-		},
-	},
+	"kylechui/nvim-surround",
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({
+			-- Configuration here, or leave empty to use defaults
+		})
+	end,
 }
