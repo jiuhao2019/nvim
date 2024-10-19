f0276883c6b4120d3e38d66f07e84de80a6df8f9
jiuhao <815514981@qq.com>
2024-10-19 11:44:57 +0800

    update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,14 @@
 vim.opt.hlsearch = true
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Keybinds to make split navigation easier.
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
@@ -22,11 +30,3 @@ vim.keymap.set("n", "<A-e>", "<cmd>Explore<cr>")
 vim.keymap.set("n", "<A-f>", "<cmd>lua require('conform').format({ async = true, lsp_fallback = true })<cr>")
 vim.keymap.set("n", "<A-s>", "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", { desc = "search" })
 vim.keymap.set("n", "<A-u>", "<cmd>UndotreeToggle<cr>")
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
